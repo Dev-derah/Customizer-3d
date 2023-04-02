e41a4c470fd43e385b41d639187044735e638d6e
dev derah <chidera261@gmail.com>
2023-04-02 10:00:49 +0100

    Added Canvas componenets

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,20 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { AnimatePresence, motion } from "framer-motion";
-import { useSnapshot } from "valtio";
+import React, { useState, useEffect } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useSnapshot } from 'valtio';
 
-import config from "../config/config";
-import state from "../store";
-import { download } from "../assets";
-import { downloadCanvasToImage, reader } from "../config/helpers";
-import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants";
-import { fadeAnimation, slideAnimation } from "../config/motion";
-import {
-  AIPicker,
-  ColorPicker,
-  CustomButton,
-  FilePicker,
-  Tab,
-} from "../components";
+import config from '../config/config';
+import state from '../store';
+import { download } from '../assets';
+import { downloadCanvasToImage, reader } from '../config/helpers';
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
+import { fadeAnimation, slideAnimation } from '../config/motion';
+import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
 const Customizer = () => {
   const snap = useSnapshot(state);
@@ -77,7 +71,6 @@ const Customizer = () => {
       setActiveEditorTab("");
     }
   };
-
   const handleDecals = (type, result) => {
     const decalType = DecalTypes[type];
 
